eecce91a2d36439aa3f5b7f57ca3b81207694cac
YOOKIKI <ny90208@naver.com>
2022-02-09 11:10:23 +0900

    20220209중간커밋

---
+++ replit/js/main.js
@@ -0,0 +1,31 @@
+// function alertSuccess(name) {
+//   alert(name + "님 로그인 성공!");
+// }
+
+// alertSuccess("김워크");
+
+// function sayHi(name){
+//     return "안녕하세요" + " " + name + "님"
+
+// }
+// const result = sayHi("김관희")
+// console.log(result, "result")
+
+function isbiggerThanHundred(myNumber) {
+   if (myNumber < 100) {
+    alert ("작다!");
+  } else if (myNumber > 100) {
+    alert ("크다!");
+  } else if (myNumber === 100){
+    alert ("같다");
+  }
+}
+
+isbiggerThanHundred(1993);
+
+// function sayHi(name){
+//     return alert ("안녕하세요" + " " + name + "님")
+
+// }
+// const result = sayHi("김관희")
+// console.log(result, "result")
