598bf006070cc6860655a624703cc248f965ff36
YOOKIKI <ny90208@naver.com>
2022-02-13 04:08:31 +0900

    add : 로그인 구현까지 완료  - 로그인 구현에 value속성문제.. 내 vs에서만 문제인것 같음.. 도움을 주는 친구의 vs에서는 문제없이 잘 value값이 이어짐..    파일 자체의 문제로 판단. js 파일을 따로 새로 열어 전체 코드 수정(html, css는 파일 내 내용 지우고 다시 시작..)    일단 로그인 구현에 대해 이해가 많이 부족한 편이라 그래도 이 이슈로 조금이나마 이해 가능해짐 울고싶었음 ^_ㅠ  - 위의 js 문제에 맞춰 HTML , CSS까지 전부 수정. 파일을 새로 열지는 않고 내용 전부 삭제 후 다시 작성.. 힘들었다 정말로...    그래도 도움을 받아 어떻게 해야 조금 더 시맨틱하게 사용 가능한지 배움 (but id는 자주 사용하지 말라고 들었건만 친구는 id를 적극적으로 사용하라고 배웠다고 함.. 어떻게해야해 이거???? 당황)
      결론 : 힘들었고.. 블로그 쓰고 자야겠다....

---
+++ westagram/main/main.js
@@ -0,0 +1,87 @@
+
+// const idInput = document.querySelector('id')
+// const pwInput = document.getElementById('pw');
+// const loginBtn = document.getElementById()
+
+// const idInput = document.getElementsById("id");
+// const pwInput = document.getElementsById("pw");
+// const loginBtn = document.getElementsByClassName("isloginButton");
+
+
+// const idInput = document.getElementById('loginId');
+// const pwInput = document.getElementById('loginPw');
+/*
+const idInput = document.getElementById('id');
+const pwInput = document.getElementById('pw');
+const loginBtn = document.getElementsByClassName('loginButton');
+
+function loginBtnClick() {
+
+  if 
+}
+
+
+function inputkeybordEvent() {
+  if (idInput.value.length > 0 && pwInput.value.length > 0) {
+    loginBtn.style.backgroundColor = rgb(47, 148, 243);
+  }
+  if (idInput.value.length > 0 && pwInput.value.length == 0) {
+    loginBtn.style.backgroundColor = "#e2f0fd";
+  }
+  if (idInput.value.length == 0 && pwInput.value.length > 0) {
+    loginBtn.style.backgroundColor = "e2f0fd";
+  }
+    
+}
+*/
+
+let id = document.getElementById("id");
+let password = document.getElementById("pw");
+let login = document.getElementById("loginButton");
+let checkString = "@";
+password.addEventListener("ketup", checkIdPassword);
+id.addEventListener("keyup", checkIdPassword);
+
+function checkIdpassword() {
+  id.ariaValueMax.
+}
+
+
+
+
+
+
+
+
+
+
+
+// userId.addEventListener("keyup", )
+
+
+// const userId = document.querySelector(".loginId");
+// const userPw = document.querySelector(".loginPw");
+// const loginBtn = document.querySelector(".isloginButton");
+
+
+// function changeId() {
+//   const btn = loginBtn.classList.contains("change");
+//   if (userId) {
+//     loginBtn.classList.add("change");
+//   }
+//   if (btn) {
+//     loginBtn.classList.remove("change");
+//   }
+// }
+
+// function changePw() {
+//   const btn = loginBtn.classList.contains("change");
+//   if (userPw) {
+//     loginBtn.classList.add("change");
+//   }
+//   if (btn) {
+//     loginBtn.classList.remove("change");
+//   }
+// }
+
+
