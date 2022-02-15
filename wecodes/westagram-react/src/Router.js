ef983226579156ff6a9afdec15377b222caee173
YOOKIKI <ny90208@naver.com>
2022-02-15 16:21:58 +0900

    add : 리팩토링 진행중 (React Router, Sass)   - Router로 첫 화면의 Login과 로그인 버튼을 이용해 Main Feed로 이동 가능한 useRouter , Link 사용   - css파일을 scss로 변환

---
+++ wecodes/westagram-react/src/Router.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import Nav from "./pages/Main/Nav/Nav";
+// import Footer from "./components/Footer/Footer";
+import Login from "./pages/Login/Login";
+import Main from "./pages/Main/Main";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      {/* <Nav /> */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Main" element={<Main />} />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  );
+}
+
+export default Router;
